29b78bdbacd08363384b0a15314b38df0c8dba99
jason.jiang <jason.jiang@mirmiles.com>
2025-08-21 09:40:46 +0800

    update db.sql

--- db.sql
+++ db.sql
@@ -16,12 +16,12 @@ CREATE TABLE `scheduler_task_config` (
     `user_id` int DEFAULT NULL COMMENT '用户ID',
     `valid` int NOT NULL DEFAULT '0' COMMENT '是否有效',
     `unique_key` varchar(100) DEFAULT NULL COMMENT '计算任务参数的唯一标识',
-    `log_storage_days` int NOT NULL DEFAULT '7' COMMENT '保留日志周期，默认7天，最长30天',
+    `log_storage_days` int NOT NULL DEFAULT '3' COMMENT '保留日志周期，默认7天，最长30天',
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (`id`),
     UNIQUE KEY `func_id` (`func_id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '调度任务配置表';
+) COMMENT = '调度任务配置表';
 
 CREATE TABLE scheduler_job_log (
     `id` BIGINT NOT NULL COMMENT '主键ID' AUTO_INCREMENT,
@@ -29,7 +29,7 @@ CREATE TABLE scheduler_job_log (
     `event` VARCHAR(20) NOT NULL COMMENT '事件类型: job_executed/job_error/log_[info|error|waring|success]',
     `scheduled_run_time` TIMESTAMP NOT NULL COMMENT '调度时间' DEFAULT CURRENT_TIMESTAMP,
     `execution_time` FLOAT COMMENT '任务执行耗时（秒）',
-    `message` TEXT NOT NULL COMMENT '日志消息',
+    `message` TEXT NULL COMMENT '日志消息',
     `created_at` TIMESTAMP NOT NULL COMMENT '创建时间' DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL COMMENT '更新时间' DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
